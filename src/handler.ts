8a545c9d9c7c6e2674a16c0c2a8621686d0ed8db
Alex Terehov <terales@users.noreply.github.com>
2021-08-24 00:50:22 +0300

    FIx CORS issue

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,14 @@
 import { unzipSync } from 'fflate';
 
-export async function handleRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url);
+const allowOrigin = 'https://app.lokalise.com';
 
+export async function handleRequest(request: Request): Promise<Response> {
+  if (request.method == 'OPTIONS') {
+    // Handle CORS preflight requests
+    return handleOptions(request);
+  }
+
+  const url = new URL(request.url);
   if (url.pathname == '/fetch-preview') {
     const params = await request.json();
 
@@ -17,6 +23,7 @@ export async function handleRequest(request: Request): Promise<Response> {
     const unpacked =  await extractFileFromBundle(bundleUrl)
 
     const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', allowOrigin);
     if (params.fileformat == 'html') {
       headers.set('Content-type', 'text/html');
     }
@@ -74,4 +81,36 @@ async function extractFileFromBundle(url: string): Promise<Uint8Array> {
       resolve(new Uint8Array());
     }
   });
+}
+
+function handleOptions(request: Request): Response {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  const reqHeaders = request.headers;
+  if (
+    reqHeaders.get('Origin') !== null &&
+    reqHeaders.get('Access-Control-Request-Method') !== null &&
+    reqHeaders.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', allowOrigin);
+    headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,OPTIONS');
+    headers.set('Access-Control-Max-Age', '86400');
+
+    // Allow all future content Request headers to go back to browser
+    // such as Authorization (Bearer) or X-Client-Name-Version
+    headers.set('Access-Control-Allow-Headers', reqHeaders.get('Access-Control-Request-Headers') ?? '');
+
+    return new Response(null, { headers });
+  }
+  else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
 }
